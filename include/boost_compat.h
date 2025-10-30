5e50b51c00a422ece6c1ef0b91874aa7620737bc
wangjh2024 <363319973@qq.com>
2025-10-30 17:09:57 +0800

    0.1

--- include/boost_compat.h
+++ include/boost_compat.h
@@ -1,23 +1,10 @@
-// boost_compat.h
 #ifndef BOOST_COMPAT_H
 #define BOOST_COMPAT_H
 
-// Boost序列化兼容性修复 - 针对Boost 1.83.0
-#define BOOST_ALLOW_DEPRECATED_HEADERS
-
-// 显式声明access类
-namespace boost {
-namespace serialization {
-    class access;
-}
-}
-
-// 包含必要的Boost序列化头文件
 #include <boost/serialization/serialization.hpp>
-#include <boost/serialization/array.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/list.hpp>
-#include <boost/serialization/singleton.hpp>
 #include <boost/serialization/access.hpp>
 
-#endif // BOOST_COMPAT_H
+// 不要重新定义 access 类，使用 Boost 提供的
+// 只提供兼容性宏定义
+
+#endif
